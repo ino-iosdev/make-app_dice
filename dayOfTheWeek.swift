907541e4d448295e8c769f7aa598ad49bb86296b
ino yang popper <ino.yang.p@gmail.com>
2024-05-25 16:43:57 -0700

    added dictionary and dayOfTheWeek

---
+++ dayOfTheWeek.swift
@@ -0,0 +1,27 @@
+
+func dayOfTheWeek(day: Int) {
+    
+    switch day {
+        
+    case 1:
+        print("Mon")
+    case 2:
+        print("T")
+    case 3:
+        print("W")
+    case 4:
+        print("TH")
+    case 5:
+        print("F")
+    case 6:
+        print("Sat")
+    case 7:
+        print("Sun")
+    default:
+        print("oops")
+        
+    }
+  
+}
+
+dayOfTheWeek(day: 5)
---
+++ dictionary.swift
@@ -0,0 +1,20 @@
+func exercise() {
+
+    //Don't change this
+    var stockTickers: [String: String] = [
+        "APPL" : "Apple Inc",
+        "HOG": "Harley-Davidson Inc",
+        "BOOM": "Dynamic Materials",
+        "HEINY": "Heineken",
+        "BEN": "Franklin Resources Inc",
+    ]
+    
+    stockTickers["WORK"] = "Slack Technologies Inc"
+    stockTickers["BOOM"] = "DMC Global Inc"
+
+     //Don't modify this
+    print(stockTickers["WORK"]!)
+    print(stockTickers["BOOM"]!)
+}
+ 
+
