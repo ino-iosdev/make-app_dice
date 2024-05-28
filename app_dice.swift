5b2db09bc9eef6f6cb679d61baea6e0315679f79
ino yang popper <ino.yang.p@gmail.com>
2024-05-28 13:07:53 -0700

    updated

--- app_dice.swift
+++ app_dice.swift
@@ -1,7 +1,14 @@
-
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "mp3")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
 
     @IBOutlet weak var imageView1: UIImageView!
     @IBOutlet weak var imageView2: UIImageView!
@@ -16,7 +23,12 @@ class ViewController: UIViewController {
 
     @IBAction func rollbutton(_ sender: UIButton) {
         
-    let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+        playSound(soundName: "diceSound")
+        
+        sender.alpha = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {sender.alpha = 1}
+        
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
     
         imageView1.image = diceArray.randomElement()!
         imageView2.image = diceArray.randomElement()!
